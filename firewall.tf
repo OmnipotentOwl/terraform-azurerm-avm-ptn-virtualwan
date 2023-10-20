4b558b580d467355d3c9b7bc0efc213cf2592fd5
OmnipotentOwl <1769881+OmnipotentOwl@users.noreply.github.com>
2023-10-20 14:52:58 -0400

    fix: Include missing properties for azurerm_firewall

--- firewall.tf
+++ firewall.tf
@@ -1,13 +1,18 @@
 resource "azurerm_firewall" "fw" {
   for_each = var.firewalls
 
+  name                = each.value.name
   location            = azurerm_virtual_hub.virtual_hub[each.value.virtual_hub_name].location
   resource_group_name = azurerm_virtual_hub.virtual_hub[each.value.virtual_hub_name].resource_group_name
+  sku_name            = each.value.sku_name
+  sku_tier            = each.value.sku_tier
+  firewall_policy_id  = each.value.firewall_policy_id
+  threat_intel_mode   = each.value.threat_intel_mode
+  zones               = each.value.zones
+  tags                = try(each.value.tags, {})
+
   virtual_hub {
-    virtual_hub_id = azurerm_virtual_hub.virtual_hub[each.value.virtual_hub_name].id
+    virtual_hub_id  = azurerm_virtual_hub.virtual_hub[each.value.virtual_hub_name].id
+    public_ip_count = each.value.vhub_public_ip_count
   }
-  name     = each.value.name
-  sku_name = each.value.sku_name
-  sku_tier = each.value.sku_tier
-  tags     = try(each.value.tags, {})
 }
